a74a84847ba16e3fd293aa0ae6596084c22fe1c2
Marck <mercercountynj@gmail.com>
2016-02-29 16:26:42 -0500

    App, Header, Order, Inventory components

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,65 @@
 var React = require('react');
 var ReactDOM  = require('react-dom');
 /*
-    STOREPICKER
+    App "main component
+*/
+
+var App = React.createClass({
+    
+    render : function() {
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header />
+                </div>
+                <Order/>
+                <Inventory/>
+            </div>
+        )
+    }
+})
+
+/*
+    Header "sub component"
+    <Header/>
+*/
+
+var Header = React.createClass({
+    render : function() {
+        return (
+            <p>Header</p>
+        )
+    }
+})
+
+/*
+    Order "sub component"
+    <Order/>
+*/
+
+var Order = React.createClass({
+    render : function() {
+        return (
+            <p>Order</p>
+        )
+    }
+})
+/*
+    Inventory "subcomponent"
+    <Order/>
+*/
+
+var Inventory = React.createClass({
+    render : function() {
+        return (
+            <p>Inventory</p>
+        )
+    }
+})
+
+
+/*
+    STOREPICKER "sub component
     This will let us make <StorePicker/>
 */
 
@@ -20,4 +78,4 @@ var StorePicker  =React.createClass({
     
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
