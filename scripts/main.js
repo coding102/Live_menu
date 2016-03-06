57c2753ceca227b43ffea1cf0de639bbd80047ff
Marck <mercercountynj@gmail.com>
2016-03-05 20:00:36 -0500

    remove button even listeners

--- scripts/main.js
+++ scripts/main.js
@@ -71,6 +71,17 @@ var App = React.createClass({
         this.state.fishes['fish-' + timestamp] = fish;
         // set the state, pass an object of what has changed
         this.setState({ fishes : this.state.fishes });
+    },
+    
+// remove item action
+removeFish : function(key) {
+    if(confirm("are you sure you want to remove this item?")) {
+    
+        this.state.fishes[key] = null;
+        this.setState({
+        fishes :this.state.fishes
+        });
+    }
 },
     
 loadSamples : function() {
@@ -92,7 +103,7 @@ loadSamples : function() {
                 </div>
                 <Order fishes={this.state.fishes} order={this.state.order} />
                 <Inventory addFish={this.addFish} loadSamples={this.loadSamples}
-                    fishes={this.state.fishes} linkState={this.linkState} />
+                    fishes={this.state.fishes} linkState={this.linkState} removeFish={this.removeFish} />
             </div>
         )
     }
@@ -199,9 +210,10 @@ var Order = React.createClass({
     renderOrder : function(key) {
         var fish = this.props.fishes[key];
         var count = this.props.order[key];
+        var removeButton = <button onClick={this.props.removeFromOrder.bind(null, key)}>&times; </button>
         
         if(!fish) {
-            return <li key={key}>Sorry, no longer available!</li>
+            return <li key={key}>Sorry, no longer available! {removeButton} </li>
         }
         
         return (
@@ -209,6 +221,7 @@ var Order = React.createClass({
                 {count}lbs
                 {fish.name}
                 <span className="price">{h.formatPrice(count * fish.price)}</span>
+                {removeButton}
             </li>
         )
     },
@@ -261,7 +274,7 @@ var Inventory = React.createClass({
                 
                 <input type="text" valueLink={linkState('fishes.' + key +'.image')} />                  
                 
-                <button>Remove Fish</button>
+                <button onClick={this.props.removeFish.bind(null, key)}>Remove Fish</button>
                 
             </div>
        ) 
