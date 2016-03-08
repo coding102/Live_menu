df662ee632d60a7a217d0c176ae1a58c98872d97
Marck <mercercountynj@gmail.com>
2016-03-07 19:54:28 -0500

    NotFound ES6 conversion

--- scripts/components/NotFound.js
+++ scripts/components/NotFound.js
@@ -3,11 +3,20 @@
 */
 import React from 'react' ;
 
-var NotFound = React.createClass({
-    render : function() {
+//var NotFound = React.createClass({
+//    render : function() {
+//        return <h1>Not Found!</h1>
+//    }
+//});
+//
+//
+//export default NotFound;
+
+//ES6 test
+class NotFound extends React.Component {
+    render() {
         return <h1>Not Found!</h1>
     }
-});
-
+}
 
 export default NotFound;
